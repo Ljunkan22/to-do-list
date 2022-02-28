8b596cffb54fe5804d4f15c23708819d1255cf6c
Ljunkan22 <erik.ljungkvist.lj@gmail.com>
2022-02-28 17:31:43 +0000

    fix the funtion selector, Add input validator

--- run.py
+++ run.py
@@ -22,33 +22,26 @@ def get_option():
     print("Input what function you want to run\n")
     print("You input should be a number between 1-5 the number corresponds to different functions\n")
     print(
-        "1\n"
+        "1: Display you to do list\n"
         "2\n"
         "3\n"
         "4\n"
         "5\n"
         )
 
-    option = input.int("Enter your data here:\n ")
+    option = int(input("Enter your data here:\n "))
 
-    options_selector(option)
+    if option > 6:
+        print(f"Your number is not between 1-5, you provided {option}!")
+        print("please try again \n")
+        get_option()
+    else:
+        options_selector(option)
 
 
-def options_selector(option):
+def display_list():
     """
-    hej
-    """
-
-    list_of_functions = [
-        get_option, display_items1, check_of_items, add_item,
-        display_items_left, remove_all_items
-        ]
-    list_of_functions[option]()
-
-
-def display_items1():
-    """
-    hej
+    Get the list from the worksheet and displays it
     """
     print("display_items")
 
@@ -79,6 +72,18 @@ def remove_all_items():
     hej
     """
     print("remove_all_items")
+
+
+def options_selector(option):
+    """
+    Use the number inputed and runs the corresponding funtion
+    """
+
+    list_of_functions = [
+        get_option, display_list, check_of_items, add_item,
+        display_items_left, remove_all_items
+        ]
+    list_of_functions[option]()
 
 
 get_option()
